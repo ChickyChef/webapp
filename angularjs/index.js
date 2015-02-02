f1e2cff3a0b9953337c7ea5263b7eab0c25d0a2f
nookskill <nookskill@hotmail.com>
2015-02-02 23:35:18 +0700

    update

--- angularjs/index.js
+++ angularjs/index.js
@@ -7,26 +7,38 @@
 
       // route for the home page
       .when('/', {
-        templateUrl : 'pages/home.html',
+        templateUrl : 'pages/home.html',  
+        pageTitle :"Home",
         controller  : 'homeCtrl'
+      })
+
+      // route for the order page
+      .when('/order', {
+        templateUrl : 'pages/order.html',
+        pageTitle :"Order",
+        controller  : 'orderCtrl'
       })
 
       // route for the about page
       .when('/about', {
         templateUrl : 'pages/about.html',
+        pageTitle :"About",
         controller  : 'aboutCtrl'
       })
 
       // route for the contact page
       .when('/contact', {
         templateUrl : 'pages/contact.html',
+        pageTitle :"Contact",
         controller  : 'contactCtrl'
       });
 
     $facebookProvider.setAppId('828635717193830').setPermissions(['email'],['user_birthday']);
   
   });
-  myApp.run(['$rootScope', '$window', function($rootScope, $window) {
+
+  //run myApp
+  myApp.run(['$rootScope', '$window','$location', function($rootScope, $window,$location) {
     (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
@@ -34,28 +46,15 @@
       js.src = "//connect.facebook.net/en_US/sdk.js";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
-    $rootScope.$on('fb.load', function() {
+
+    $rootScope.$on('fb.load','$routeChangeSuccess', function(event, current, previous) {
+      $rootScope.title = current.$$route.pageTitle;
       $window.dispatchEvent(new Event('fb.load'));
     });
   }])
 
   // create the controller and inject Angular's $scope
-  
-  myApp.controller('mainCtrl', function($scope,$location){
-        $scope.isActive = function(route) {
-        return route === $location.path();
-    };
-  });
-  
-  myApp.controller('aboutCtrl', function($scope) {
-    $scope.message = 'Look! I am an about page.';
-  });
-
-  myApp.controller('contactCtrl', function($scope) {
-    $scope.message = 'Contact us! ChickyChef by Q. This is just a demo.';
-  });
-
-  myApp.controller('homeCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+  myApp.controller('mainCtrl', ['$scope', '$facebook','$location', function($scope, $facebook,$l) {
     $scope.$on('fb.auth.authResponseChange', function() {
       $scope.status = $facebook.isConnected();
       if($scope.status) {
@@ -66,12 +65,38 @@
     });
 
     $scope.loginToggle = function() {
-      if($scope.status) {
+    if ($scope.status) {
         $facebook.logout();
-      } else {
+    } else {
         $facebook.login();
-      }
+    }
     };
 
+
   }]);
+
+
+  myApp.controller('navController',function ($scope, $location) 
+  { 
+      $scope.isActive = function (viewLocation) { 
+          return viewLocation === $location.path();
+      };
+  });
+  
+  myApp.controller('homeCtrl', function($scope) {
+
+  });
+
+  myApp.controller('orderCtrl', function($scope) {
+    $scope.message = 'เชิญสั่งรายการอาหารครับ';
+  });
+
+  myApp.controller('aboutCtrl', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+  });
+
+  myApp.controller('contactCtrl', function($scope) {
+    //$scope.message = 'Contact us! ChickyChef by Q. This is just a demo.';
+  });
+
 
